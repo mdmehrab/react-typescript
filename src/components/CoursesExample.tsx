1755a81dd84a9a83be364d6702256d4678248b45
rakib <mehrabishraqhossian@gmail.com>
2024-11-07 18:54:13 +0600

    chor: using map

--- src/components/CoursesExample.tsx
+++ src/components/CoursesExample.tsx
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import { skillsList } from "../data";
 
 const CoursesExample = () => {
   const [activeTab, setActiveTab] = useState("Web Development");
@@ -12,186 +13,71 @@ const CoursesExample = () => {
         From critical skills to technical topics, Udemy supports your
         professional development.
       </p>
-      <div className=" border-b border-gray-400">
+      <div className="border-b border-gray-400">
         <ul
           className="flex flex-wrap text-base font-bold text-center text-gray-700"
           role="tablist"
         >
-          <li className="" role="presentation">
-            <button
-              className={`inline-block p-4 border-b-2 rounded-t-lg ${
-                activeTab === "Web Development" ? "border-black" : ""
-              }`}
-              type="button"
-              onClick={() => setActiveTab("Web Development")}
-            >
-              Web Development
-            </button>
-          </li>
-          <li className="me-2" role="presentation">
-            <button
-              className={`inline-block p-4 border-b-2 rounded-t-lg ${
-                activeTab === " IT Certifications" ? "border-black" : ""
-              }`}
-              type="button"
-              onClick={() => setActiveTab(" IT Certifications")}
-            >
-              IT Certifications
-            </button>
-          </li>
-          <li className="me-2" role="presentation">
-            <button
-              className={`inline-block p-4 border-b-2 rounded-t-lg ${
-                activeTab === "Leadership" ? "border-black" : ""
-              }`}
-              type="button"
-              onClick={() => setActiveTab("Leadership")}
-            >
-              Leadership
-            </button>
-          </li>
-          <li role="presentation">
-            <button
-              className={`inline-block p-4 border-b-2 rounded-t-lg ${
-                activeTab === "Data Science" ? "border-black" : ""
-              }`}
-              type="button"
-              onClick={() => setActiveTab("Data Science")}
-            >
-              Data Science
-            </button>
-          </li>
-
-          <li role="presentation">
-            <button
-              className={`inline-block p-4 border-b-2 rounded-t-lg ${
-                activeTab === "Communication" ? "border-black" : ""
-              }`}
-              type="button"
-              onClick={() => setActiveTab("Communication")}
-            >
-              Communication
-            </button>
-          </li>
-
-          <li role="presentation">
-            <button
-              className={`inline-block p-4 border-b-2 rounded-t-lg ${
-                activeTab === "Business Analyttics & intelligence"
-                  ? "border-black"
-                  : ""
-              }`}
-              type="button"
-              onClick={() => setActiveTab("Business Analyttics & intelligence")}
-            >
-              Business Analyttics & intelligence
-            </button>
-          </li>
+          {skillsList?.map((skill) => (
+            <li key={skill.title} role="presentation">
+              <button
+                className={`inline-block p-4 border-b-2 rounded-t-lg ${
+                  activeTab === skill.title
+                    ? "border-black"
+                    : "border-transparent"
+                }`}
+                type="button"
+                onClick={() => setActiveTab(skill.title)}
+              >
+                {skill.title}
+              </button>
+            </li>
+          ))}
         </ul>
       </div>
-      <div id="default-tab-content">
-        <div
-          className={`p-4 rounded-lg bg-gray-50 dark:bg-gray-800 ${
-            activeTab === "Web Development" ? "" : "hidden"
-          }`}
-        >
-          <ul className="flex flex-wrap gap-3">
-            <li className=" bg-gray-300 p-3 rounded-full">
-              <button>
-                <span className="font-bold text-black">Web Development</span>
-                <p className="text-gray-600">17.7M+Learners</p>
-              </button>
-            </li>
 
-            <li className=" bg-gray-300 p-3 rounded-full">
-              <button>
-                <span className="font-bold text-black">JavaScript</span>
-                <p className="text-gray-600">13.3M+Learners</p>
-              </button>
-            </li>
+      {/* Display items under the active tab */}
+      <div className="py-4">
+        {skillsList?.map(
+          (skill) =>
+            activeTab === skill.title && (
+              <div
+                key={skill.title}
+                className="flex items-center justify-between"
+              >
+                {skill.items?.map((item, index) => (
+                  <div
+                    key={index}
+                    className="p-4 bg-gray-100 rounded-lg shadow-md"
+                  >
+                    <h3 className="text-lg font-semibold">{item.text}</h3>
+                  </div>
+                ))}
+              </div>
+            )
+        )}
+      </div>
 
-            <li className=" bg-gray-300 p-3 rounded-full">
-              <button>
-                <span className="font-bold text-black">React JS</span>
-                <p className="text-gray-600">7M+Learners</p>
-              </button>
-            </li>
-
-            <li className=" bg-gray-300 p-3 rounded-full">
-              <button>
-                <span className="font-bold text-black">Anglur</span>
-                <p className="text-gray-600">4M+Learners</p>
-              </button>
-            </li>
-
-            <li className=" bg-gray-300 p-3 rounded-full">
-              <button>
-                <span className="font-bold text-black">Java</span>
-                <p className="text-gray-600">15.5M+Learners</p>
-              </button>
-            </li>
-
-            <li className=" bg-gray-300 p-3 rounded-full">
-              <button>
-                <span className="font-bold text-black">
-                  Android Development
-                </span>
-                <p className="text-gray-600">8M+Learners</p>
-              </button>
-            </li>
-
-            <li className=" bg-gray-300 p-3 rounded-full">
-              <button>
-                <span className="font-bold text-black">IOS Development</span>
-                <p className="text-gray-600">4M+Learners</p>
-              </button>
-            </li>
-
-            <li className=" bg-gray-300 p-3 rounded-full">
-              <button>
-                <span className="font-bold text-black">CSS</span>
-                <p className="text-gray-600">9M+Learners</p>
-              </button>
-            </li>
-          </ul>
-        </div>
-        <div
-          className={`p-4 rounded-lg bg-gray-50 dark:bg-gray-800 ${
-            activeTab === " IT Certifications" ? "" : "hidden"
-          }`}
-        >
-          <p>first</p>
-        </div>
-        <div
-          className={`p-4 rounded-lg bg-gray-50 dark:bg-gray-800 ${
-            activeTab === "Leadership" ? "" : "hidden"
-          }`}
-        >
-          <p>first</p>
-        </div>
-        <div
-          className={`p-4 rounded-lg bg-gray-50 dark:bg-gray-800 ${
-            activeTab === "Data Science" ? "" : "hidden"
-          }`}
-        >
-          <p>last</p>
-        </div>
-
-        <div
-          className={`p-4 rounded-lg bg-gray-50 dark:bg-gray-800 ${
-            activeTab === "Communication" ? "" : "hidden"
-          }`}
-        >
-          second last
-        </div>
-        <div
-          className={`p-4 rounded-lg bg-gray-50 dark:bg-gray-800 ${
-            activeTab === "Business Analyttics & intelligence" ? "" : "hidden"
-          }`}
-        >
-          {" "}
-          third last
-        </div>
+      {/* cards */}
+       <div className="py-4">
+        {skillsList?.map(
+          (skill) =>
+            activeTab === skill.title && (
+              <div
+                key={skill.title}
+                className="flex items-center justify-between"
+              >
+                {skill.cardItems?.map((item, index) => (
+                  <div
+                    key={index}
+                    className="p-4 bg-gray-100 rounded-lg shadow-md"
+                  >
+                    <h3 className="text-lg font-semibold">{item.currentPrice}</h3>
+                  </div>
+                ))}
+              </div>
+            )
+        )}
       </div>
     </div>
   );
--- src/data.ts
+++ src/data.ts
@@ -1,24 +1,170 @@
 export const footerLinks = [
-    [
-        { id: 1, content: { English: 'Udemy Business', Bngl: 'উডেমি ব্যবসা', Chin: '优达学城' }, url: '/' },
-        { id: 2, content: { English: 'Teach on Udemy', Bangla: 'উডেমিতে পড়ান', China: '在优达学城教学' }, url: '/' },
-        { id: 3, content: { English: 'Get the app', Bangla: 'অ্যাপটি ডাউনলোড করুন', China: '下载应用程序' }, url: '/' },
-        { id: 4, content: { English: 'About us', Bangla: 'আমাদের সম্পর্কে', China: '关于我们' }, url: '/' },
-        { id: 5, content: { English: 'Contact us', Bangla: 'যোগাযোগ করুন', China: '联系我们' }, url: '/' },
-    ],
-    [
-        { id: 1, content: { English: 'Careers', Bangla: 'ক্যারিয়ার', China: '职业' }, url: '/' },
-        { id: 2, content: { English: 'Blog', Bangla: 'ব্লগ', China: '博客' }, url: '/' },
-        { id: 3, content: { English: 'Help and Support', Bangla: 'সাহায্য এবং সহায়তা', China: '帮助与支持' }, url: '/' },
-        { id: 4, content: { English: 'Affiliate', Bangla: 'অ্যাফিলিয়েট', China: '联盟计划' }, url: '/' },
-        { id: 5, content: { English: 'Investors', Bangla: 'বিনিয়োগকারীরা', China: '投资者' }, url: '/' },
-    ],
-    [
-        { id: 1, content: { English: 'Terms', Bangla: 'শর্তাবলী', China: '条款' }, url: '/' },
-        { id: 2, content: { English: 'Privacy policy', Bangla: 'গোপনীয়তা নীতি', China: '隐私政策' }, url: '/' },
-        { id: 3, content: { English: 'Cookie settings', Bangla: 'কুকি সেটিংস', China: 'Cookie设置' }, url: '/' },
-        { id: 4, content: { English: 'Sitemap', Bangla: 'সাইটম্যাপ', China: '网站地图' }, url: '/' },
-        { id: 5, content: { English: 'Accessibility statement', Bangla: 'অ্যাক্সেসযোগ্যতার বিবৃতি', China: '可及性声明' }, url: '/' },
-    ]
+  [
+    {
+      id: 1,
+      content: {
+        English: "Udemy Business",
+        Bngl: "উডেমি ব্যবসা",
+        Chin: "优达学城",
+      },
+      url: "/",
+    },
+    {
+      id: 2,
+      content: {
+        English: "Teach on Udemy",
+        Bangla: "উডেমিতে পড়ান",
+        China: "在优达学城教学",
+      },
+      url: "/",
+    },
+    {
+      id: 3,
+      content: {
+        English: "Get the app",
+        Bangla: "অ্যাপটি ডাউনলোড করুন",
+        China: "下载应用程序",
+      },
+      url: "/",
+    },
+    {
+      id: 4,
+      content: {
+        English: "About us",
+        Bangla: "আমাদের সম্পর্কে",
+        China: "关于我们",
+      },
+      url: "/",
+    },
+    {
+      id: 5,
+      content: {
+        English: "Contact us",
+        Bangla: "যোগাযোগ করুন",
+        China: "联系我们",
+      },
+      url: "/",
+    },
+  ],
+  [
+    {
+      id: 1,
+      content: { English: "Careers", Bangla: "ক্যারিয়ার", China: "职业" },
+      url: "/",
+    },
+    {
+      id: 2,
+      content: { English: "Blog", Bangla: "ব্লগ", China: "博客" },
+      url: "/",
+    },
+    {
+      id: 3,
+      content: {
+        English: "Help and Support",
+        Bangla: "সাহায্য এবং সহায়তা",
+        China: "帮助与支持",
+      },
+      url: "/",
+    },
+    {
+      id: 4,
+      content: {
+        English: "Affiliate",
+        Bangla: "অ্যাফিলিয়েট",
+        China: "联盟计划",
+      },
+      url: "/",
+    },
+    {
+      id: 5,
+      content: {
+        English: "Investors",
+        Bangla: "বিনিয়োগকারীরা",
+        China: "投资者",
+      },
+      url: "/",
+    },
+  ],
+  [
+    {
+      id: 1,
+      content: { English: "Terms", Bangla: "শর্তাবলী", China: "条款" },
+      url: "/",
+    },
+    {
+      id: 2,
+      content: {
+        English: "Privacy policy",
+        Bangla: "গোপনীয়তা নীতি",
+        China: "隐私政策",
+      },
+      url: "/",
+    },
+    {
+      id: 3,
+      content: {
+        English: "Cookie settings",
+        Bangla: "কুকি সেটিংস",
+        China: "Cookie设置",
+      },
+      url: "/",
+    },
+    {
+      id: 4,
+      content: { English: "Sitemap", Bangla: "সাইটম্যাপ", China: "网站地图" },
+      url: "/",
+    },
+    {
+      id: 5,
+      content: {
+        English: "Accessibility statement",
+        Bangla: "অ্যাক্সেসযোগ্যতার বিবৃতি",
+        China: "可及性声明",
+      },
+      url: "/",
+    },
+  ],
 ];
 
+export const skillsList = [
+  {
+    id: 1,
+    title: 'web development',
+    items: [
+        {id: 1, text: 'aaaa'},
+        {id: 1, text: 'aaaa'},
+        {id: 1, text: 'aaaa'},
+        {id: 1, text: 'aaaa'},
+        {id: 1, text: 'aaaa'},
+        {id: 1, text: 'aaaa'},
+        {id: 1, text: 'aaaa'},
+        {id: 1, text: 'aaaa'},
+        {id: 1, text: 'aaaa'},
+        {id: 1, text: 'aaaa'},
+        {id: 1, text: 'aaaa'},
+        {id: 1, text: 'aaaa'},
+        {id: 1, text: 'aaaa'},
+        {id: 1, text: 'aaaa'},
+        {id: 1, text: 'aaaa'},
+        {id: 1, text: 'aaaa'},
+    ],
+    cardItems : [
+        {id: 1, imgSrc: '', title: "", subTitle: "", rating: '4', totalRating: '3453', currentPrice: '43', beforePrice: "34", mostSell: 'Bestseller'},
+        {id: 1, imgSrc: '', title: "", subTitle: "", rating: '4', totalRating: '3453', currentPrice: '43', beforePrice: "34", mostSell: 'Bestseller'},
+        {id: 1, imgSrc: '', title: "", subTitle: "", rating: '4', totalRating: '3453', currentPrice: '43', beforePrice: "34", mostSell: 'Bestseller'},
+        {id: 1, imgSrc: '', title: "", subTitle: "", rating: '4', totalRating: '3453', currentPrice: '43', beforePrice: "34", mostSell: 'Bestseller'},
+        {id: 1, imgSrc: '', title: "", subTitle: "", rating: '4', totalRating: '3453', currentPrice: '43', beforePrice: "34", mostSell: 'Bestseller'},
+    ]
+  },
+  {
+    id: 1,
+    title: 'web Services',
+    items: [
+        {id: 1, text: 'bbb'},
+        {id: 1, text: 'bbb'},
+    ],
+    cardItems : [
+        {id: 1, imgSrc: '', title: "", subTitle: "", rating: '4', totalRating: '3453', currentPrice: '43', beforePrice: "34", mostSell: 'Bestseller'}
+    ]
+  }
+];
