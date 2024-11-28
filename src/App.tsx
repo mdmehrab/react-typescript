1ec65106dbfcf8977a6484941fb5bcff175d1f77
rakib <mehrabishraqhossian@gmail.com>
2024-11-28 16:44:54 +0600

    last commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,12 @@
 const App = () => {
   return (
     <>
-      <div className=" md:mx-auto bg-red-700">
-        <p>kasjfakfaskjfkjsfk</p>
+      <div className="relative h-32 w-32 bg-blue-gray-50 ">
+        <div className="absolute bottom-0 left-0 h-16 w-16 flex items-center justify-center text-black bg-blue-gray-600">
+          01
+        </div>
       </div>
+      <div>kfls</div>
     </>
   );
 };
