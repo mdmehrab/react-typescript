a7a4ba255f40f9c0b982eca6ee0e72353c33bf4e
rakib <mehrabishraqhossian@gmail.com>
2024-11-29 17:50:35 +0600

    user table

--- src/pages/Register.tsx
+++ src/pages/Register.tsx
@@ -1,5 +1,4 @@
 import { useState } from "react";
-import Axios from "axios";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import axios from "axios";
