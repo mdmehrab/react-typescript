9e9c4e25489cc24203539dc9e7e01601c5c6a2e2
rakib <mehrabishraqhossian@gmail.com>
2024-11-30 18:42:12 +0600

    works with cookie

--- src/features/auth/authSlice.ts
+++ src/features/auth/authSlice.ts
@@ -1,26 +1,22 @@
 import { createSlice, PayloadAction } from '@reduxjs/toolkit';
 
 interface AuthState {
-  isAuthenticated: boolean;
-  user: { id: string; name: string } | null;
+
 }
 
 const initialState: AuthState = {
-  isAuthenticated: false,
-  user: null,
+
 };
 
 const authSlice = createSlice({
   name: 'auth',
   initialState,
   reducers: {
-    login: (state, action: PayloadAction<{ id: string; name: string }>) => {
-      state.isAuthenticated = true;
-      state.user = action.payload;
+    login: (state, action: PayloadAction) => {
+     
     },
     logout: (state) => {
-      state.isAuthenticated = false;
-      state.user = null;
+   
     },
   },
 });
--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -5,21 +5,12 @@ import { FaApple } from "react-icons/fa6";
 import axios from "axios";
 import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
-import { useDispatch } from 'react-redux';
-import { AppDispatch } from '../features/store/store';
-import { login } from '../features/auth/authSlice';
 
 const Login = () => {
   const [loginCredentials, setLoginCredetials] = useState({
     email: "",
     password: "",
   });
-
-  const dispatch = useDispatch<AppDispatch>();
-
-  const handleLogin = () => {
-    dispatch(login({ id: '1', name: 'John Doe' }));
-  };
 
   const handleLoginChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     const { name, value } = e.target;
@@ -36,18 +27,21 @@ const Login = () => {
     try {
       const response = await axios.post(
         `${import.meta.env.VITE_API_URL}/users/login`,
-        loginCredentials
+        loginCredentials,
+        { withCredentials: true }
       );
 
-      
-      if (response) {
+      if (response && response?.data) {
+        // Set the access token in a cookie
+
         toast.success("Login successful!");
+        console.log(response?.data);
       }
 
       setLoginCredetials({
         email: "",
-        password: ""
-      })
+        password: "",
+      });
     } catch (err) {
       toast.error("Login failed. Please check your credentials.");
       console.log(err);
@@ -55,95 +49,92 @@ const Login = () => {
   };
 
   return (
-
     <>
-    <div className="w-full bg-white p-8">
-      <div className="flex">
-        <div>
-          <img
-            src="https://frontends.udemycdn.com/components/auth/desktop-illustration-step-1-x1.webp"
-            alt="Login illustration"
-          />
-        </div>
-        <div className="w-full max-w-md mx-auto mt-14">
-          <div className="font-bold text-3xl ">
-            <p className="justify-center flex">Log in to your Udemy</p>
-            <p className="justify-center flex">account</p>
-          </div>
-
-          <div className="mb-4 mt-6">
-            <input
-              placeholder="Email"
-              type="email"
-              name="email"
-              value={loginCredentials.email}
-              onChange={handleLoginChange}
-              className="w-full p-4 border border-black placeholder-black font-bold text-xs"
-              required
+      <div className="w-full bg-white p-8">
+        <div className="flex">
+          <div>
+            <img
+              src="https://frontends.udemycdn.com/components/auth/desktop-illustration-step-1-x1.webp"
+              alt="Login illustration"
             />
           </div>
+          <div className="w-full max-w-md mx-auto mt-14">
+            <div className="font-bold text-3xl ">
+              <p className="justify-center flex">Log in to your Udemy</p>
+              <p className="justify-center flex">account</p>
+            </div>
 
-          <div className="mb-4">
-            <input
-              placeholder="Password"
-              type="password"
-              name="password"
-              value={loginCredentials.password}
-              onChange={handleLoginChange}
-              className="w-full p-4 border border-black placeholder-black font-bold text-xs"
-              required
-            />
-          </div>
+            <div className="mb-4 mt-6">
+              <input
+                placeholder="Email"
+                type="email"
+                name="email"
+                value={loginCredentials.email}
+                onChange={handleLoginChange}
+                className="w-full p-4 border border-black placeholder-black font-bold text-xs"
+                required
+              />
+            </div>
 
-          <button
-            className="bg-purple-500 font-bold text-white p-3 w-full"
-            onClick={handleLoginSubmit}
-          >
-            Log in
-          </button>
+            <div className="mb-4">
+              <input
+                placeholder="Password"
+                type="password"
+                name="password"
+                value={loginCredentials.password}
+                onChange={handleLoginChange}
+                className="w-full p-4 border border-black placeholder-black font-bold text-xs"
+                required
+              />
+            </div>
 
-          <div className="flex justify-center mt-4 gap-1">
-            or
-            <button className="text-purple-900 underline font-bold underline-offset-4">
-              Forgot Password?
+            <button
+              className="bg-purple-500 font-bold text-white p-3 w-full"
+              onClick={handleLoginSubmit}
+            >
+              Log in
             </button>
-          </div>
 
-          <div className="mt-6 flex justify-center">
-            <span className="text-gray-600">Other login options</span>
-          </div>
-
-          <div className="flex space-x-4 mt-4 justify-center">
-            <button className="border border-black p-3 hover:bg-gray-200">
-              <FcGoogle size={24} />
-            </button>
-            <button className="border border-black p-3 hover:bg-gray-200">
-              <BiLogoFacebookCircle size={24} />
-            </button>
-            <button className="border border-black p-3 hover:bg-gray-200">
-              <FaApple size={24} />
-            </button>
-          </div>
-
-          <div className="bg-gray-100 mt-10 py-2">
-            <p className="flex justify-center border border-b-gray-400 py-3 ">
-              Don't have an account?{" "}
-              <button className="text-purple-800 font-bold underline underline-offset-4">
-                Sign up
+            <div className="flex justify-center mt-4 gap-1">
+              or
+              <button className="text-purple-900 underline font-bold underline-offset-4">
+                Forgot Password?
               </button>
-            </p>
-            <span className="flex justify-center mt-2 pb-2 text-purple-900 font-bold underline underline-offset-4">
-              <button>Log in with your organization</button>
-            </span>
+            </div>
+
+            <div className="mt-6 flex justify-center">
+              <span className="text-gray-600">Other login options</span>
+            </div>
+
+            <div className="flex space-x-4 mt-4 justify-center">
+              <button className="border border-black p-3 hover:bg-gray-200">
+                <FcGoogle size={24} />
+              </button>
+              <button className="border border-black p-3 hover:bg-gray-200">
+                <BiLogoFacebookCircle size={24} />
+              </button>
+              <button className="border border-black p-3 hover:bg-gray-200">
+                <FaApple size={24} />
+              </button>
+            </div>
+
+            <div className="bg-gray-100 mt-10 py-2">
+              <p className="flex justify-center border border-b-gray-400 py-3 ">
+                Don't have an account?{" "}
+                <button className="text-purple-800 font-bold underline underline-offset-4">
+                  Sign up
+                </button>
+              </p>
+              <span className="flex justify-center mt-2 pb-2 text-purple-900 font-bold underline underline-offset-4">
+                <button>Log in with your organization</button>
+              </span>
+            </div>
           </div>
         </div>
-      </div>
 
-      {/* Toast Notifications */}
-      <ToastContainer />
-    </div>
-    
-    <button onClick={handleLogin}>Login</button>
+        {/* Toast Notifications */}
+        <ToastContainer />
+      </div>
     </>
   );
 };
